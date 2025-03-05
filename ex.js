9ec707d8d62573fa94650a510fae0ff408ba4b56
Bao171 <baonlg5327@ut.edu.vn>
2025-03-05 20:19:23 +0700

    ex1-5

--- ex.js
+++ ex.js
@@ -1,3 +1,31 @@
-
+//ex 1
+let mathScore = 8;
+let physicsScore = 7;
+let chemistryScore = 9;
+let averageScore =(8+7+9)/3;
+console.log("Average score:", averageScore);
+//ex2
+let width = 5;
+let height = 3;
+let perimeter =(width+height )*2;
+let area = width*height;
+console.log("Perimeter:", perimeter);
+console.log("Area:", area);
+//ex3
+let celsius = 30;
+let fahrenheit=(celsius*1.8)+32;
+console.log(celsius + "°C = " + fahrenheit + "°F");
+//ex4
+let billAmount = 200000;
+let tipPercent = 10;
+let tipAmount = (billAmount * tipPercent) / 100;
+let totalAmount = billAmount + tipAmount;
+console.log("Bill amount:", billAmount);
+console.log("Tip amount:", tipAmount);
+console.log("Total amount:", totalAmount);
+//ex5
+let number = 7;
+let isEven = number % 2 ===0;
+console.log("Number " + number + " is " + (isEven ? "even" : "odd"));
 
 
